12e7219199b07f201b6297baa322097df00cf43c
destrat18 <destrat18@gmail.com>
2024-10-17 10:03:00 +0000

    add

--- example.py
+++ example.py
@@ -4,29 +4,44 @@ from pysmt.typing import BOOL, REAL
 from wmipa import WMI
 from wmipa.integration import LatteIntegrator
 from wmipa.integration import VolestiIntegrator
+from wmipa.integration import PPIntegrator
+
 
 # variables definition
 x = Symbol("x", REAL)
+y = Symbol("y", REAL)
 
 # formula definition
 # fmt: off
-phi = And(GE(x, Real(1)),
-          LE(x, Real(2)))
+phi = And(
+    GE(x, Real(1)),
+    LE(x, Real(2)),
+    GE(y, Real(1)),    
+    LE(y, Real(2)),
+    )
 
 # weight function definition
-w = x
+w = x+y
 # fmt: on
 
-chi = And(GE(x, Real(1)),
-          LE(x, Real(2)))
+chi = Bool(True)
 
 print("Formula:", phi.serialize())
 print("Weight function:", w.serialize())
 print("Support:", chi.serialize())
 
 print()
-for mode in [WMI.MODE_ALLSMT, WMI.MODE_PA, WMI.MODE_SA_PA, WMI.MODE_SAE4WMI]:
-    for integrator in (LatteIntegrator(), VolestiIntegrator()):
+for mode in [
+    WMI.MODE_ALLSMT, 
+    # WMI.MODE_PA, 
+    # WMI.MODE_SA_PA, 
+    # WMI.MODE_SAE4WMI
+    ]:
+    for integrator in (
+        LatteIntegrator(), 
+        VolestiIntegrator(),
+        # PPIntegrator(),
+        ):
         try:
             wmi = WMI(chi, w, integrator=integrator)
             result, n_integrations = wmi.computeWMI(phi, mode=mode)
