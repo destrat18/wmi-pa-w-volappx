1da4f66ffd031377b170eabf969c64526b34c863
destrat18 <destrat18@gmail.com>
2024-12-23 05:27:29 +0000

    add examples

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ from pysmt.shortcuts import Iff, Implies, Ite, Symbol
 from pysmt.typing import BOOL, REAL
 # from wmipa.integration import VolestiIntegrator, LatteIntegrator
 from wmipa.integration import FazaIntegrator
+import argparse
 
 from wmipa import WMI
 import time
@@ -40,26 +41,39 @@ chi = (x >= 0) & (x <= 3) & (y >= 0) & (y <= 4)
 print("Weight function:", w.serialize())
 print("Support:", chi.serialize())
 
-for integrator in [
-        # VolestiIntegrator(), 
-        # LatteIntegrator(), 
-        FazaIntegrator(max_workers=8, threshold=0.5)
-    ]:
-    for mode in [
-            WMI.MODE_SAE4WMI
+
+if __name__ == "__main__":
+    
+    
+    parser = argparse.ArgumentParser(
+        prog='Mega Miners',
+        description='I am approximating!'
+        )
+        
+    parser.add_argument("--degree", help="Handelman degree", type=int, default=None)
+    parser.add_argument("--max-workers", help="Number of workers", type=int, default=1)
+    parser.add_argument("--threshold", help="Error threshold", type=float, default=0.001)
+    
+    args = parser.parse_args()
+    
+    for integrator in [
+            FazaIntegrator(max_workers=16, threshold=0.5)
         ]:
-        try:
-            start_time = time.time()
-            wmi = WMI(chi, w, integrator=integrator)
-            result, n_integrations = wmi.computeWMI(phi, mode=mode)
-            print(
-                "WMI with mode {}, \t integrator = {}, \t result = {}, \t # integrations = {}, \t time = {:.2f}s({:.2f}h)".format(
-                    mode, integrator.__class__.__name__, result, n_integrations, time.time()-start_time,(time.time()-start_time)/3600
+        for mode in [
+                WMI.MODE_SAE4WMI
+            ]:
+            try:
+                start_time = time.time()
+                wmi = WMI(chi, w, integrator=integrator)
+                result, n_integrations = wmi.computeWMI(phi, mode=mode)
+                print(
+                    "WMI with mode {}, \t integrator = {}, \t result = {}, \t # integrations = {}, \t time = {:.2f}s({:.2f}h)".format(
+                        mode, integrator.__class__.__name__, result, n_integrations, time.time()-start_time,(time.time()-start_time)/3600
+                    )
                 )
-            )
-        except Exception as e:
-            print(
-                "WMI with mode {}, \t integrator = {}, \t failed = {}".format(
-                    mode, integrator.__class__.__name__, str(e)
+            except Exception as e:
+                print(
+                    "WMI with mode {}, \t integrator = {}, \t failed = {}".format(
+                        mode, integrator.__class__.__name__, str(e)
+                    )
                 )
-            )
