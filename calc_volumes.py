3f2a85cf1852766b7ec145593eaeb12ec10f26ec
destrat18 <destrat18@gmail.com>
2024-12-25 14:32:54 +0000

    add addaptive degree

--- calc_volumes.py
+++ calc_volumes.py
@@ -66,6 +66,8 @@ if __name__ == "__main__":
                 
                 if stats['error'] > 0:
                     break
+                    
+                total_degree += 1
             
         
         except Exception as e:
