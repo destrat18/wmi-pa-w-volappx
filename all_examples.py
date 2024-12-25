dee422c4c31458f79f4551e47a551d162d9b1c06
destrat18 <destrat18@gmail.com>
2024-12-25 04:25:29 +0000

    add sorted and more examples

--- all_examples.py
+++ all_examples.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     results = []
 
-    for f_name in os.listdir(examples_path):
+    for f_name in sorted(os.listdir(examples_path)):
         try:
             example = SourceFileLoader(f_name.split(".")[0], os.path.join(examples_path, f_name)).load_module()
             phi = example.phi
@@ -35,64 +35,60 @@ if __name__ == "__main__":
             print("Weight function:", w.serialize())
             print("Support:", chi.serialize())
             
-            faza_integrator = FazaIntegrator(max_workers=args.max_workers, threshold=args.threshold)
+            # faza_integrator = FazaIntegrator(max_workers=args.max_workers, threshold=args.threshold)
             
-            for integrator in [
-                LatteIntegrator(),
-                VolestiIntegrator(),
-                faza_integrator,
-            ]:
-                for mode in [
-                        WMI.MODE_SAE4WMI
-                    ]:
-                    start_time = time.time()
-                    try:
-                        faza_integrator.logs = []
-                        wmi = WMI(chi, w, integrator=integrator)
-                        volume, n_integrations = wmi.computeWMI(phi, mode=mode)
-                        total_time = time.time()-start_time
-                        print(
-                            "WMI with mode {}, \t integrator = {}, \t volume = {}, \t # integrations = {}, \t time = {:.2f}s({:.2f}h)".format(
-                                mode, integrator.__class__.__name__, volume, n_integrations, total_time,(total_time)/3600
-                            )
-                        )
+            # for integrator in [
+            #     LatteIntegrator(),
+            #     VolestiIntegrator(),
+            #     faza_integrator,
+            # ]:
+            #     for mode in [
+            #             WMI.MODE_SAE4WMI
+            #         ]:
+            #         start_time = time.time()
+            #         try:
+            #             faza_integrator.logs = []
+            #             wmi = WMI(chi, w, integrator=integrator)
+            #             volume, n_integrations = wmi.computeWMI(phi, mode=mode)
+            #             total_time = time.time()-start_time
+            #             print(
+            #                 "WMI with mode {}, \t integrator = {}, \t volume = {}, \t # integrations = {}, \t time = {:.2f}s({:.2f}h)".format(
+            #                     mode, integrator.__class__.__name__, volume, n_integrations, total_time,(total_time)/3600
+            #                 )
+            #             )
                         
-                        results.append(
-                            {
-                                'time': total_time,
-                                'example': f_name,
-                                'integrator': integrator.__class__.__name__,
-                                'mode': mode,
-                                'result': volume,
-                                'n_integrations': n_integrations,
-                                'logs': faza_integrator.logs
-                            }
-                        )
+            #             results.append(
+            #                 {
+            #                     'time': total_time,
+            #                     'example': f_name,
+            #                     'integrator': integrator.__class__.__name__,
+            #                     'mode': mode,
+            #                     'result': volume,
+            #                     'n_integrations': n_integrations,
+            #                     'logs': faza_integrator.logs
+            #                 }
+            #             )
                         
-                    except Exception as e:
-                        print(
-                            "WMI with mode {}, \t integrator = {}, \t failed = {}".format(
-                                mode, integrator.__class__.__name__, str(e)
-                            )
-                        )
-                        logging.exception(e)
-                        results.append(
-                            {
-                                'time': time.time()-start_time,
-                                'example': f_name,
-                                'integrator': integrator.__class__.__name__,
-                                'mode': mode,
-                                'result': str(e),
-                                'n_integrations': None,
-                                'logs': faza_integrator.logs
-                            }
-                        )
+            #         except Exception as e:
+            #             print(
+            #                 "WMI with mode {}, \t integrator = {}, \t failed = {}".format(
+            #                     mode, integrator.__class__.__name__, str(e)
+            #                 )
+            #             )
+            #             logging.exception(e)
+            #             results.append(
+            #                 {
+            #                     'time': time.time()-start_time,
+            #                     'example': f_name,
+            #                     'integrator': integrator.__class__.__name__,
+            #                     'mode': mode,
+            #                     'result': str(e),
+            #                     'n_integrations': None,
+            #                     'logs': faza_integrator.logs
+            #                 }
+            #             )
                     
-                    pd.DataFrame(results).to_csv(f"example_results_{int(time.time())}.csv", index=False)
-                    # with open('example_results.json', 'w') as output_f:
-                    #     print(results)
-                    #     json.dump(results, output_f, indent = 6)
-                
+            #         pd.DataFrame(results).to_csv(f"example_results_{int(time.time())}.csv", index=False)
                 
                 
         except Exception as e:
